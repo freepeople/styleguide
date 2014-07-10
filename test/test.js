0baa47d416e2f6e43f24698cc073406df3a49edc
David Annopolsky <dannopolsky@freepeople.com>
2014-07-10 17:08:51 -0400

    completed dropdown menu test

--- test/test.js
+++ test/test.js
@@ -7,37 +7,37 @@ var browser = new Browser({
     debug: true
 });
 
+var dropDownExists = function() {
+    test('dropdowns exists', function(t) {
+        t.ok(browser.queryAll('.dropdowns').length > 0, 'one or more dropdowns on the page');
+        t.end();
+    });
+};
+
+var dropDownWorks = function() {
+    test('dropdown menu', function(t) {
+        t.plan(4);
+        t.false(browser.query('.dropdown.is-opened'), 'hidden');
+        browser
+            .clickLink('.dropdown-trigger')
+            .then(function() {
+                t.true(browser.query('.dropdown.is-opened'), 'opened');
+                return browser.clickLink('.dropdown-menu a:first-child');
+            })
+            .then(function() {
+                t.equal(browser.query('.dropdown-trigger').textContent, 'Hello There', 'first dropdown item selected');
+                t.false(browser.query('.dropdown.is-opened'), 'closed');
+            });
+    });
+};
+
+
 // Anything we cannot resolve such as the request
 // or a DOM issue catch it all here
-browser.on("error", function(error) {
-    console.error(error);
-});
-
-
 browser
-    .visit("https://rawgit.com/freepeople/styleguide/master/index.html")
-    .then(function() {
-        test('dropdowns exists', function(t) {
-            t.ok(browser.query('.dropdowns'), 'one or more dropdowns on the page');
-            t.end();
-        });
+    .on("error", function(error) {
+        console.error(error);
     })
-    .then(function() {
-        test('dropdown menu', function(t) {
-            t.plan(3);
-            // Dropdown should be hidden at first
-            t.false(browser.query('.dropdown.is-opened'), 'dropdown hidden');
-            // After first click dropdown should be opened
-            // with a class named is-opened
-            // after 2nd click dropdown should be closed again
-            browser
-                .clickLink('.dropdown-trigger')
-                .then(function() {
-                    t.true(browser.query('.dropdown.is-opened'), 'dropdown visible');
-                    return browser.clickLink('.dropdown-trigger');
-                })
-                .then(function() {
-                    t.false(browser.query('.dropdown.is-opened'), 'dropdown closed');
-                });
-        });
-    });
+    .visit("https://rawgit.com/freepeople/styleguide/master/index.html")
+    .then(dropDownExists)
+    .then(dropDownWorks);
